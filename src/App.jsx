9e4e335a0b2d0deab2a003a8173a65275d34fd1b
tdsc.user <johnnydicksoncode@gmail.com>
2024-08-02 13:35:12 -0500

    PM(setup): Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './scss/styles.scss'
+import * as bootstrap from 'bootstrap'
+
+export default function App() {
+  return (
+    <>
+      <button class='btn btn-primary'>Load Bootstrap</button>
+    </>
+  )
+}
